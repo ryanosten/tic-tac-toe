921d6f77939a48627576bcde6af6d2116fc6cae0
Ryan Osten <ostenrj@gmail.com>
2016-12-18 17:19:32 -0500

    lastest commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-$(document).ready(function(){
-
+	$(document).ready(function(){
 	//track state of board. 0 = box has not been claimed by a player. 1 = box claimed by 'o'. 2 = box claimed by 'x'.
 	var board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
@@ -7,6 +6,7 @@ $(document).ready(function(){
 	var winView = '<div class="screen screen-win" id="finish">';
 		winView +='<header>';
 		winView += '<h1>Tic Tac Toe</h1>';
+		winView += '<div class="player-name"></div>';
 		winView += '<p class="message">Winner</p>';
 		winView += '<a href="#" class="button">New game</a>';
 		winView += '</header></div>';
@@ -16,33 +16,70 @@ $(document).ready(function(){
 
 	//keep track of # of moves made
 	var moves = 0;
+	
+	var aiGame = false;
+	var playerOneName;
+	var playerTwoName;
+
+	var startGame = {
+		
+		gameType: function(){
+			
+			var playComputer = prompt('Do you want to play against the computer? Yes or no?');
+
+			if (playComputer.toLowerCase() !== 'yes' && playComputer.toLowerCase() !== 'no'){
+				
+				alert('You must choose if you want to play against computer. Please respond Yes or No.');
+				
+				startGame.gameType();
+			
+			} else if (playComputer.toLowerCase() === 'yes'){
+				
+				startGame.aiGame();
+			
+			} else {
+				
+				startGame.humanGame();
+			}
+		},
+
+		humanGame: function(){
+			aiGame = false;
+			playerOneName = prompt('Player One, please enter your name');
+			playerTwoName = prompt('Player Two, please enter your name');
+			
+		},
+
+		aiGame: function(){
+			aiGame = true;
+			playerOneName = prompt('Player One, please enter your name');
+			playerTwoName = 'Tic Tac Bot';
+		}
+	}
 
 	function nextTurn(playerWent, playerGo){
-		//remove classive .active on player who just moved
+		//remove class .active on player who just moved
 		$(playerWent).removeClass('active');
 		//add class active on player whose turn is next
 		$(playerGo).addClass('active');
-	}
+	};
 
-  	function whoWon(boxCheck){
-  		$('#start').after(winView);
-  		$('#board').hide();
+	function whoWon(boxCheck){
+		$('#start').after(winView);
+		$('#board').hide();
 
-  		if(board[boxCheck] === 1){
-			$('.screen-win').addClass('screen-win-one');
-  		} else if (board[boxCheck] === 2) {
-			$('.screen-win').addClass('screen-win-two');
-  		} else {
-  			$('.screen-win').addClass('screen-win-tie');
-  			$('.message').text("It's a tie!");
+		if(board[boxCheck] === 1){
+		$('.screen-win').addClass('screen-win-one');
+		$('.player-name').html(playerOneName);
+		} else if (board[boxCheck] === 2) {
+		$('.screen-win').addClass('screen-win-two');
+		$('.player-name').html(playerTwoName);
+		} else {
+			$('.screen-win').addClass('screen-win-tie');
+			$('.message').text("It's a tie!");
 
-  		}
-
-  		//click handler for start new game after first game completed
-		$('#finish a.button').on('click', function(){
-			startNextGame();
-		});
-  	}
+		}
+	};
 
 	function checkWin(){
 
@@ -79,7 +116,13 @@ $(document).ready(function(){
 				whoWon(2);
 
 			} else if(moves === 9){
+				
 				whoWon(10); 
+			
+			} else {
+				
+				return false;
+			
 			}
 	}
 	
@@ -91,23 +134,47 @@ $(document).ready(function(){
 
 		$('#player1').addClass('active');
 		$('#player2').removeClass('active');
+
+		startGame.gameType();
+
 		$('.screen-win').remove();
 		$('#board').show();
+		$('#playerOneName').html(playerOneName);
+		$('#playerTwoName').html(playerTwoName);
+		
 		board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 		turn = 1;
 		moves = 0;
 	}
+	
+	function miniMax(){
+		
+	}
+
+	//click handler for start new game after first game completed. We add it here bc element binding to isn't added to DOM until 1st game ends.
+	$(document).on('click', '#finish a.button', function(){
+			startNextGame();
+		});
 
 	//click handler on 'start game' button. If clicked, then start game!
 	$('.screen-start .button').on('click', function(){
+		
+		startGame.gameType();
+		
+		$('#playerOneName').html(playerOneName);
+		$('#playerTwoName').html(playerTwoName);
+
 		//hide the start screen
 		$('.screen-start').hide();
+			
 		//remove the 'hide' class on board element to display the game board
 		$('.board').removeClass('hide');
-	});
 
-	//indicate in UI that first turn belongs to player 1
-	$('#player1').addClass('active');
+		//indicate in UI that first turn belongs to player 1
+		$('#player1').addClass('active');
+	
+	});
+	
 	//mouseover handler for boxes
 	$('.box').on('mouseover', function(){
 		
@@ -124,6 +191,7 @@ $(document).ready(function(){
 		} else if(turn === 2 && board[boxHovered] === 0) {
 			$(this).css('background-image', 'url(img/x.svg)')
 		}
+
 	});
 
 	//click handler for boxes
@@ -142,13 +210,13 @@ $(document).ready(function(){
 			$(this).addClass('box-filled-1');
 			
 			//change turn indicator
-			nextTurn('#player1', '#player2')
+			nextTurn('#player1', '#player2');
 
 			//change turn to 'x' turn
 			turn = 2;
 
 			//increment the moves variable
-			moves++
+			moves++;
 
 			//check for a win or tie
 			checkWin();
@@ -163,17 +231,18 @@ $(document).ready(function(){
 			$(this).addClass('box-filled-2');
 			
 			//change turn indicator
-			nextTurn('#player2', '#player1')
+			nextTurn('#player2', '#player1');
 
 			//change turn to 'o' turn
 			turn = 1;
 
 			//increment the moves variable	
-			moves++
+			moves++;
 
 			//check for a win or tie
 			checkWin();
 		}
 	});
 });
+
 
