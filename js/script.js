ec898a236bde9b3a600691bd8f3df38435aa121c
Ryan Osten <ostenrj@gmail.com>
2017-01-07 11:14:44 -0500

    implemented dumb ai

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-	$(document).ready(function(){
+(function(window, $){
 	//track state of board. 0 = box has not been claimed by a player. 1 = box claimed by 'o'. 2 = box claimed by 'x'.
 	var masterBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
@@ -17,14 +17,17 @@
 	//keep track of # of moves made
 	var moves = 0;
 	
+	// check if aiGame
 	var aiGame = false;
-	var aiScore = 0;
+
+	//store player names
 	var playerOneName;
 	var playerTwoName;
-	var clone_winner;
 
-	var startGame = {
+	//game object
+	var game = {
 		
+		//function for letting user choose human game or ai game	
 		gameType: function(){
 			
 			var playComputer = prompt('Do you want to play against the computer? Yes or no?');
@@ -33,18 +36,19 @@
 				
 				alert('You must choose if you want to play against computer. Please respond Yes or No.');
 				
-				startGame.gameType();
+				game.gameType();
 			
 			} else if (playComputer.toLowerCase() === 'yes'){
 				
-				startGame.aiGame();
+				game.aiGame();
 			
 			} else {
 				
-				startGame.humanGame();
+				game.humanGame();
 			}
 		},
 
+		//function for getting player names in human game
 		humanGame: function(){
 			aiGame = false;
 			playerOneName = prompt('Player One, please enter your name');
@@ -52,189 +56,68 @@
 			
 		},
 
+		//function for getting player name if ai game
 		aiGame: function(){
 			aiGame = true;
 			playerOneName = prompt('Player One, please enter your name');
 			playerTwoName = 'Tic Tac Bot';
 		}
-	}
-
-	//function to get the index of max value in an array
-	function indexofMax(arr) {
-
-			var max = arr[0];
-			var maxIndex = 0;
-
-			for (var i = 1; i < arr.length; i++){
-				if(arr[i] > max){
-					maxIndex = i;
-					max = arr[i];
-				}
-			}
-
-			return maxIndex;
-		}
+	};
 
 	function aiGo(){
 		//get the current boardstate
-		var cloneBoard = [1,1,2,0,2,0,0,0,1];
-		var scores_list = [];
-		var moves_list = [];
-		var currentMove;
-		var playerTurn = 2;
-		var stateList =[];
-		var newState;
+		var cloneBoard = masterBoard;
+		//store the available moves
+		var available_moves = [];
+		//create an array from the .box list items
+		var boxes = $('.box');
 
-		//get minimax of cloneBoard
 
-		minimax(cloneBoard)
-
-		function(miniMax){}
-
-		//create an array of possible new board states
-		//var boardStates = [];
-
-		/*
-
+		//loop through the board and get available moves
 		for(i = 0; i < cloneBoard.length; i++){
-			//create the boardstate
-			newState = cloneBoard.slice(0);
-			
-			//if space is unclaimed
-			if (newState[i] === 0){
-				
-				//make a move to the empty space
-				newState[i] = 2;
-				
-				//store the new state after the move
-				stateList.push(newState);				
-
-				//store the current move
-				currentMove = [i];
-				
-				//push the move to the moves list
-				moves_list.push(currentMove);
-				
-				//check if its terminal
-				//minimax(newState, 2);
-				console.log('99 ran');
+			if(cloneBoard[i] === 0){
+				available_moves.push(i);
 			}
-		
-		}		
-		
-		console.log(stateList);
-
-		for(h = 0; h < stateList.length; h++){
-				console.log('loop ran to get minimax on move ' + h);
-				var minimax = miniMax(stateList[h], playerTurn);
-				console.log('108 ran');
-				console.log('minimax returned', minimax);
-				scores_list.push(minimax);
-				console.log('110 ran');
 		}
 
+		//generate a random move 
+		var move = available_moves[Math.floor(Math.random()*available_moves.length)];
+
+		//change state of board array to indicate the state of box clicked. 2 indicates that the box belongs to 'x'
+		masterBoard[move] = 2;
+
+		// add 'x' box-filled-2 class
+		$(boxes[move]).addClass('box-filled-2');
 		
-		//loop through the scores_list and get the index of the max value
-		var max_score_index = indexofMax(scores_list);
-		
-		//get the move at the same index as the highest score in scores_list
-		var best_move = moves_list[max_score_index];
+		//change turn indicator
+		nextTurn('#player2', '#player1');
 
-		//make the move//
+		//change turn to 'o' turn
+		turn = 1;
 
+		//increment the moves variable	
+		moves++;
 
-		function miniMax(state, player){
+		//check for a win or tie
+		checkWin(masterBoard);
 
-			console.log('running minimax on ', state);
-
-			//is state terminal?
-			if (player % 2 === 0 && checkWin(state, true) === true){
-					
-					console.log('x wins, returned 10');
-					return 10;
-
-
-			} else if (player % 2 !== 0 && checkWin(state, true) === true){
-					console.log('o wins, returned -10')
-					return -10;
-
-			} else if (state.indexOf(0) === -1){
-					console.log('its a tie')
-					return 0;
-			
-			//if no, generate new moves
-			} else {
-					
-					var playersTurn = player;
-					var local_statelist = [];
-					var local_scores = [];
-					
-					//change playersTurn to player who just went to pass that into minimax
-					playersTurn ++;
-
-					//in this state, game is not over, so let's create new states
-					for(i = 0; i < state.length; i++){
-						console.log("create new states and loop through");
-						//store a new state
-						newState = state.slice(0);
-						//if space is is empty
-						if (newState[i] === 0){
-							
-							//and if player 1's turn 
-							if(playersTurn % 2 !== 0){
-								//make player 1 move
-								newState[i] = 1;
-							
-							//if player 2's turn
-							} else if (playersTurn % 2 === 0) {
-								//make player 2 move
-								newState[i] = 2;
-								
-							
-							}
-							console.log('newState is ', newState);
-							 
-							local_statelist.push(newState);
-							console.log('local_statelist is ', local_statelist)
-						}
-					}
-
-					
-
-					for (j = 0; j < local_statelist.length; j++){
-						console.log('run minimax on new states')
-						var minimax = miniMax(local_statelist[j], playersTurn);
-						console.log('push minimax to new local_scores');
-						local_scores.push(minimax);
-						console.log('local scores array is ', local_scores);
-				}
-				//scores_list.push(local_scores);
-				//console.log(scores_list)
-				//console.log(moves)
-				
-			}
-		//check if state is terminal
-			//if yes, return the utility and store it in an array
-			//if no, check if its max or mins turn, if it is max turn, then return the maximum for all possible actions (maximum for being in state s and taking action a)
-		}
-
-	};
-	*/
-
-
-
+	}
 
 	function nextTurn(playerWent, playerGo){
 		//remove class .active on player who just moved
 		$(playerWent).removeClass('active');
 		//add class active on player whose turn is next
 		$(playerGo).addClass('active');
-	};
+	}
 
+	//check who won
 	function whoWon(boxCheck){
+			
+			//show win view and hide board
 			$('#start').after(winView);
 			$('#board').hide();
 
+			//check which playervalue was is the box that triggered checkWin to indicate a win
 			if(masterBoard[boxCheck] === 1){
 				$('.screen-win').addClass('screen-win-one');
 				$('.player-name').html(playerOneName);
@@ -245,99 +128,53 @@
 				$('.screen-win').addClass('screen-win-tie');
 				$('.message').text("It's a tie!");
 			}
-		};
+		}
 
+	//check if there is a win or tie
+	function checkWin(masterBoard){
 
-	function checkWin(whichBoard, aiSimulation){
-
-			console.log('checking whoWon on', whichBoard);
-
-			if(whichBoard[0] === whichBoard[1] && whichBoard[1] === whichBoard[2] && whichBoard[0] !== 0){
-				
-				console.log('there is a winner')
-				if(aiSimulation === false){
-					whoWon(0);
-				}
-				
-				return true;
-				
-			} else if(whichBoard[3] === whichBoard[4] && whichBoard[4] === whichBoard[5] && whichBoard[3] !== 0) {
-
-				console.log('there is a winner')
-
-				if(aiSimulation === false){
-					whoWon(3);
-				}
-				return true;
-
-			} else if(whichBoard[6] === whichBoard[7] && whichBoard[7] === whichBoard[8] && whichBoard[6] !== 0) {
-				
-				console.log('there is a winner')
-
-				if(aiSimulation === false){
-					whoWon(6);
-				}
-				return true;
-
-			} else if(whichBoard[0] === whichBoard[3] && whichBoard[3]=== whichBoard[6] && whichBoard[0] !== 0) {
-				
-				console.log('there is a winner')
-
-				if(aiSimulation === false){
-					whoWon(0);
-				}
-				return true;
+		if(masterBoard[0] === masterBoard[1] && masterBoard[1] === masterBoard[2] && masterBoard[0] !== 0){
 			
-			} else if(whichBoard[1] === whichBoard[4] && whichBoard[4] === whichBoard[7] && whichBoard[1] !== 0) {
+			//if there is a win, call whoWon function to see which player won
+			whoWon(0);
 
-				console.log('there is a winner')
+		} else if(masterBoard[3] === masterBoard[4] && masterBoard[4] === masterBoard[5] && masterBoard[3] !== 0) {
 
-				if(aiSimulation === false){
-					whoWon(1);
-				}
-				return true;
+			whoWon(3);
 
-			} else if(whichBoard[2] === whichBoard[5] && whichBoard[5] === whichBoard[8] && whichBoard[2] !== 0) {
-				
-				console.log('there is a winner')
-
-
-				if(aiSimulation === false){
-					whoWon(2);
-				}
-				return true;
-
-			} else if(whichBoard[0] === whichBoard[4] && whichBoard[4] === whichBoard[8] && whichBoard[0] !== 0) {
-				
-				console.log('there is a winner')
-
-				if(aiSimulation === false){
-					whoWon(0);
-				}
-				return true;
-
-			} else if(whichBoard[2] === whichBoard[4] && whichBoard[4] === whichBoard[6] && whichBoard[2] !== 0) {
-				
-				console.log('there is a winner')
-
-				if(aiSimulation === false){
-					whoWon(2);
-				}
-				return true;
-
-				//clone still has an issue with ties
-			} else if(aiSimulation === false && moves === 9){
-
-				console.log('its a tie')
-
-				whoWon(10);
-				return true; 
+		} else if(masterBoard[6] === masterBoard[7] && masterBoard[7] === masterBoard[8] && masterBoard[6] !== 0) {
 			
-			} 
+			whoWon(6);
 
-			console.log('there is no winner');
+		} else if(masterBoard[0] === masterBoard[3] && masterBoard[3]=== masterBoard[6] && masterBoard[0] !== 0) {
+
+			whoWon(0);
+		
+		} else if(masterBoard[1] === masterBoard[4] && masterBoard[4] === masterBoard[7] && masterBoard[1] !== 0) {
+
+			whoWon(1);
+
+		} else if(masterBoard[2] === masterBoard[5] && masterBoard[5] === masterBoard[8] && masterBoard[2] !== 0) {
+			
+			whoWon(2);
+
+		} else if(masterBoard[0] === masterBoard[4] && masterBoard[4] === masterBoard[8] && masterBoard[0] !== 0) {
+			
+			whoWon(0);
+
+		} else if(masterBoard[2] === masterBoard[4] && masterBoard[4] === masterBoard[6] && masterBoard[2] !== 0) {
+
+			whoWon(2);
+
+
+		} else if(moves === 9){
+
+			whoWon(10);
+		
+		} 
 	}
 	
+	//function for starting a new game after the initial game has ended
 	function startNextGame(){
 		$('.box').each(function(){
 			$(this).removeClass('box-filled-1 box-filled-2');
@@ -347,7 +184,7 @@
 		$('#player1').addClass('active');
 		$('#player2').removeClass('active');
 
-		startGame.gameType();
+		game.gameType();
 
 		$('.screen-win').remove();
 		$('#board').show();
@@ -367,7 +204,7 @@
 	//click handler on 'start game' button. If clicked, then start game!
 	$('.screen-start .button').on('click', function(){
 		
-		startGame.gameType();
+		game.gameType();
 		
 		$('#playerOneName').html(playerOneName);
 		$('#playerTwoName').html(playerTwoName);
@@ -386,7 +223,7 @@
 	//mouseover handler for boxes
 	$('.box').on('mouseover', function(){
 		
-		var boxHovered = $(this).index()
+		var boxHovered = $(this).index();
 
 		//remove all hover state background images
 		$('.box').css('background-image', '');
@@ -397,7 +234,7 @@
 		
 		//if turn variable is not equal to 1, and box not claimed, then display the x svg on mouseover
 		} else if(turn === 2 && masterBoard[boxHovered] === 0) {
-			$(this).css('background-image', 'url(img/x.svg)')
+			$(this).css('background-image', 'url(img/x.svg)');
 		}
 
 	});
@@ -427,15 +264,14 @@
 			moves++;
 
 			//check for a win or tie
-			checkWin(masterBoard, false);
+			checkWin(masterBoard);
 
-			//if game is against the ai, initiate the ai turn
+			//if game is against the ai, run aiGo
 			if(aiGame === true){
 				aiGo();
-				turn = 1;
 			}
 		
-		//if it is 'x' turn, and box not claimed then...
+		//if not an ai Game, and it is 'x' turn, and box not claimed then...
 		} else if(turn === 2 && masterBoard[boxClicked] === 0){
 			
 			//change state of board array to indicate the state of box clicked. 2 indicates that the box belongs to 'x'
@@ -454,9 +290,10 @@
 			moves++;
 
 			//check for a win or tie
-			checkWin(masterBoard, false);
+			checkWin(masterBoard);
 		}
 	});
-});
+})(window, jQuery);
+
 
 
